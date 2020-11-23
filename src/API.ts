d5dc35e595b756d306c6067d84457d34b2d638e1
Ivo Krastev <ivo.krustev@yahoo.com>
2020-11-23 12:49:48 +0200

    first commit

---
+++ src/API.ts
@@ -0,0 +1,15 @@
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endPoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const response = await fetch(endPoint);
+  const data = await response.json();
+  console.log(data);
+};
